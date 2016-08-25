508fc5b7b62f7cd8bd3ef70d71becc98b7fa624c
codemuhammed <codemuhammed@gmail.com>
2016-08-25 15:35:25 +0100

    Synchronized memberlist to firebase.

--- public/js/components/members/members.js
+++ public/js/components/members/members.js
@@ -9,7 +9,6 @@ angular.module('membersComponent' , [])
 .directive('members' , function(){
     return{
        link:function(scope , elem , attrs){
-           console.log(attrs);
            scope.ref = attrs.ref;
            scope.member = scope.$eval(attrs.member);
        },
@@ -23,7 +22,17 @@ angular.module('membersComponent' , [])
 */
 .controller('membersController' , function($scope , $timeout, fireservice) {
      $timeout(function() {
-        
+
+         //Listens for when membersList is updated on firebase
+         fireservice.syncMembers($scope.ref).then(null , null , function(membersList) {
+            if(angular.isDefined(membersList)) {
+              console.log(membersList);
+            }  
+         });
+
+         //Register this member on memberlist on firebase
+         fireservice.registerMember($scope.member);
+
      });
 
 });
