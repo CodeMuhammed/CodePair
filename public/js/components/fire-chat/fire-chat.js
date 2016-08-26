760086590bb9a0d9ba7de92fa12d86dd34321f35
codemuhammed <codemuhammed@gmail.com>
2016-08-26 07:15:29 +0100

    Indenting code to meetup with standard

--- public/js/components/fire-chat/fire-chat.js
+++ public/js/components/fire-chat/fire-chat.js
@@ -1,40 +1,42 @@
 /**
- *Handles the instance of chat that syncs to firebase //{}[]
+ * @description
+ *
+ * Handles the chatting segment of the pair programming sesssion and synchronizes with firebase
 */
 
 angular.module('chatComponent' , [])
 
-//
+//This is a component directive
 .directive('fireChat' , function(){
-    return{
-       link:function(scope , elem , attrs){
-          scope.ref = attrs.ref;
-          scope.member = attrs.member;
-       },
-       templateUrl:'js/components/fire-chat/fire-chat.html',
-       controller: 'chatController'
-    };
+  return{
+    link:function(scope , elem , attrs){
+      scope.ref = attrs.ref;
+      scope.member = attrs.member;
+    },
+    templateUrl:'js/components/fire-chat/fire-chat.html',
+    controller: 'chatController'
+  };
 })
 
 //This controller implements the functionality of the chat interface and syncs the data to firebase
 .controller('chatController' , function($scope , $timeout , fireservice) {
+  $timeout(function() {
+    fireservice.syncChat($scope.ref , function(chatHistory) {
+      $scope.chatHistory = Object.values(chatHistory);
+    });
+  });
 
-   $timeout(function() {
-     fireservice.syncChat($scope.ref , function(chatHistory) {
-        $scope.chatHistory = Object.values(chatHistory);
-     });
-   });
+  //This posts a new chat message to the history
+  $scope.postMessage = function(postText) {
+    //
+    if(postText.length > 0) {
+      var newPost = {
+        text:postText,
+        username:$scope.member.username,
+        posted: Date.now()
+      };
+      fireservice.postChat(newPost);
+    }
+  }
 
-   //This posts a new chat message to the history
-   $scope.postMessage = function(postText) {
-     //
-     if(postText.length > 0) {
-       var newPost = {
-         text:postText,
-         username:$scope.member.username,
-         posted: Date.now()
-       };
-       fireservice.postChat(newPost);
-     }
-   }
 });
